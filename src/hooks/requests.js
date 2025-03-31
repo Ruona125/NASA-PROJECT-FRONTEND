cbe62337af878516dd7482674e48a3f333a476b4
Ruona125 <ogheneruonaagadagba4@gmail.com>
2025-03-31 21:47:28 +0100

    added new api rul

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -1,49 +1,49 @@
-const API_URL = "https://nasa-mission-control-api-production.up.railway.app/v1";
+const API_URL = 'https://nasa-mission-control-api-production.up.railway.app/v1'
 
 // Load planets and return as JSON.
-async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
-  return await response.json();
+async function httpGetPlanets () {
+  const response = await fetch(`${API_URL}/planets`)
+  return await response.json()
 }
 
 // Load launches, sort by flight number, and return as JSON.
-async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches = await response.json();
+async function httpGetLaunches () {
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchedLaunches = await response.json()
   return fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber;
-  });
+    return a.flightNumber - b.flightNumber
+  })
 }
 
 // Submit given launch data to launch system.
-async function httpSubmitLaunch(launch) {
+async function httpSubmitLaunch (launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
-      method: "post",
+      method: 'post',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(launch),
-    });
+      body: JSON.stringify(launch)
+    })
   } catch (err) {
     return {
-      ok: false,
-    };
+      ok: false
+    }
   }
 }
 
 // Delete launch with given ID.
-async function httpAbortLaunch(id) {
+async function httpAbortLaunch (id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
-      method: "delete",
-    });
+      method: 'delete'
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
     return {
-      ok: false,
-    };
+      ok: false
+    }
   }
 }
 
-export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch }
